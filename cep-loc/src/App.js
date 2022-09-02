58f1ae971da56c299c8323150067bf2176431250
Wittallo M. Araujo <wittallo@edu.uniube.br>
2022-09-02 05:30:57 -0300

    Update pj1
    ainda têm bugs

--- cep-loc/src/App.js
+++ cep-loc/src/App.js
@@ -1,61 +1,61 @@
 import React from 'react';
 import { useState } from 'react';
+import { render } from 'react-dom';
 import { FiSearch } from 'react-icons/fi';
 import api from './services/api';
 
-function App  () {
-  (
-   
+function App () {
+    render () (
+  
+            const  [input, setInput] = useState('');
+            const  [cep, setCep] = useState({});
 
-    const [input, setInput] = useState('');
-    const [cep, setCep] = useState({});
+              (function handleSearch() {
+              if(input === ''){
+                alert('Digite um número de Cep válido')
+                
+              }
+              try {
+                const response =  api.get(`${input}/json`);
+                setCep(response.data)
+                setInput('');
+                
+              } catch (error) {
+                alert('Opps, Algo deu errado, tente novamente');
+                setInput('');
+                
+              }
+            }
+            (
+              <div className='container'>
+              <h1 className='Title'>Buscador de Cep</h1>
 
-    async function handleSearch (){
-      if(input === ''){
-        alert('Digite um número de Cep válido')
-        return;
-      }
-      try {
-        const response = await api.get(`${input}/json`);
-        setCep(response.data)
-        setInput('');
-        
-      } catch (error) {
-        alert('Opps, Algo deu errado, tente novamente');
-        setInput('');
-        
-      }
-    }
-    return (
-      <div className='container'>
-       <h1 className='Title'>Buscador de Cep</h1>
+              <div className='container-input'>
+                    
+                    <input type='text' placeholder="Digite o Cep..."
+                    value={input} onChange={(bc) => setInput(bc.target.value) } />
+              <button className='bsearch' id='Pesquisa'>
+                <FiSearch size={25} color='black' />
+              </button>
+              </div>
+              {Object.keys(cap),length > 0 && (
+                
+                <main className='cont-principal'>
+                    <h2>CEP: {cep.cep}</h2>
+                    <span>Rua: {cep.logradouro}</span>
+                    <span>Complemento: {cep.Complemento}</span>
+                    <span>Bairro: {cep.bairro}</span>
+                    <spam>Cidade/UF: {cep.localidade}</spam>
+                  </main>
 
-       <div className='container-input'>
+              )};
+                
+
+              </div>
+
             
-            <input type='text' placeholder="Digite o Cep..."
-            value={input} onChange={(bc) => setInput(bc.target.value) } />
-       <button className='bsearch'>
-        <FiSearch size={25} color='black' />
-       </button>
-       </div>
-       {Object.keys(cap),length > 0 && (
-        
-        <main className='cont-principal'>
-            <h2>CEP: {cep.cep}</h2>
-            <span>Rua: {cep.logradouro}</span>
-            <span>Complemento: {cep.Complemento}</span>
-            <span>Bairro: {cep.bairro}</span>
-            <spam>Cidade/UF: {cep.localidade}</spam>
-          </main>
-
+            )
        )}
-         
-
-      </div>
-
-     
-  );
-       }
 
 
-export default App;
+    export default App;
