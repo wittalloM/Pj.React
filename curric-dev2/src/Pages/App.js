433a843ee03d90cc812c2eab6a70829821d9241b
wittalloM <wittallo@edu.uniube.br>
2022-09-12 06:45:46 -0300

    Att3
    Adição do novo projeto curridev2

---
+++ curric-dev2/src/Pages/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Title from "../Title";
+
+function Home() {
+  return(
+
+  <body>
+  <p>No começo e sempre fácil</p>
+ <Title>né</Title>
+  </body>
+
+
+
+    )};
+
+export default Home;
---
+++ curric-dev2/src/Title/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+
+const Title = () => (
+ <h1>Dificil viu</h1>
+
+);
+
+export default Title;
