743f4c6d4505a9e0ffbffbae6c8c59d06fb10258
Wittallo M. Araujo <111251838+wittalloM@users.noreply.github.com>
2022-11-20 11:09:15 -0300

    Atualização no Projeto P-Dev

--- pf-dev2/src/App.js
+++ pf-dev2/src/App.js
@@ -1,6 +1,11 @@
 import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import { BsFillFileCodeFill } from 'react-icons/bs';
+import {AiFillContacts} from 'react-icons/ai';
+
 import './App.css';
+import Sobre from './Pages/Sobre';
 import Home from './Pages/Home';
+
 
 function App() {
   return (
@@ -8,11 +13,12 @@ function App() {
     <>
     
         <ul className='naveg'>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/Sobre">Sobre</Link></li>
+          <li className='iten'><Link to="/"><BsFillFileCodeFill classname='icon1' size={28}></BsFillFileCodeFill> Home</Link></li>
+          <li className='iten'><Link to="/Sobre"><AiFillContacts size={28}></AiFillContacts>Sobre</Link></li>
         </ul>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/Sobre" element={<Sobre />} />
         </Routes>
     
     <div className="App">
